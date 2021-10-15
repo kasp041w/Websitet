eee4bb33490f65df9b26bcf3a33fd3b85962ebd1
kasp041w <kasp041w@stud.kea.dk>
2021-10-16 00:20:53 +0200

    v1 site

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#product_section",
+    scrub: 1,
+    start: "center bottom",
+    end: "+=25"
+  }
+});
+
+tl.from(".scroll_fx", {
+  y: 100,
+  opacity: 0,
+  
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#raad_section",
+    scrub: 1,
+    start: "right bottom",
+    end: "+=25"
+  }
+});
+
+tl.from(".scroll_fx2", {
+  x: 100,
+  opacity: 0,
+  
+});
